85417f82310f049dd208204355f87bf1b27d82ff
Roman Kaniuka <kanuka878@gmail.com>
2023-05-26 00:41:32 +0300

    Added Identity

---
+++ Notes/Areas/Identity/Data/NotesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Notes.Areas.Identity.Data;
+
+namespace Notes.Data;
+
+public class NotesDbContext : IdentityDbContext<User>
+{
+    public NotesDbContext(DbContextOptions<NotesDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Notes/Program.cs
+++ Notes/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Notes.Areas.Identity.Data;
+using Notes.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("NotesDbContextConnection") ?? throw new InvalidOperationException("Connection string 'NotesDbContextConnection' not found.");
+
+builder.Services.AddDbContext<NotesDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<NotesDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +27,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
